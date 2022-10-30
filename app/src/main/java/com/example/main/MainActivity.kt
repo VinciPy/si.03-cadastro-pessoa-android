035c3a6580e8383c1af4a5778b4d41c7801c151c
Tronaluz <lucas_tron@hotmail.com>
2022-10-30 19:58:04 -0300

    Update.1.0.8

--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private  TextView text_tela_cadastro;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide();
+    }
+
+    private void InicarComponentes(){
+
     }
 }
