54542c317b38c1be3921ad1d263c0bf7a718997a
VinciPy <viniciusantana323@gmail.com>
2022-10-30 20:33:43 -0300

    fix: errors in activity

--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -5,15 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    private  TextView text_tela_cadastro;
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide();
-    }
-
-    private void InicarComponentes(){
-
     }
 }
