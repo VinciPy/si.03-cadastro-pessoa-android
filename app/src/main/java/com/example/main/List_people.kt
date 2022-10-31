22d97d3915b3e911819295c33ce685ee0c8ba240
Tronaluz <lucas_tron@hotmail.com>
2022-10-30 21:51:11 -0300

    Update.1.1.1

--- app/src/main/java/com/example/main/List_people.kt
+++ app/src/main/java/com/example/main/List_people.kt
@@ -1,13 +1,11 @@
-
 package com.example.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class FormCadastro : AppCompatActivity() {
+class List_people : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_cadastro)
-        supportActionBar?.hide();
+        setContentView(R.layout.activity_list_people)
     }
 }
